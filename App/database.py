2d7bad69c4abd6c32faad16ab76c6d38dc1fc6b0
carlos-dani-dev <carlos.rocha@ufpi.edu.br>
2026-02-12 11:12:24 -0300

    Fix database.py

--- App/database.py
+++ App/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from .config import SQLALCHEMY_DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
