61821871eff47b1022b8fabb287f35ebe21307e0
carlos-dani-dev <carlos.rocha@ufpi.edu.br>
2026-02-12 11:40:00 -0300

    Fix startup db creation 2

--- App/__init__.py
+++ App/__init__.py
@@ -1,10 +1,8 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from .config import SQLALCHEMY_DATABASE_URL
+from .question import Question
+from .answer import Answer
+from .response import Response
+from .survey import Survey
+from .question_option import QuestionOption
+from .answer_option import AnswerOption
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+from .base import Base
